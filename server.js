3dbbfcc9ce1c557ca84cd7bda5056dcb7eafddd8
nyirurugo omar <nyirurugoomar@gmail.com>
2023-08-07 08:56:52 +0200

    initial commit

---
+++ server.js
@@ -0,0 +1,29 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const articleRouter = require('./routes/articles')
+const app = express()
+
+
+mongoose.connect('mongodb://localhost/blog',{ useNewUrlParser:true, useUnifiedTopology:true})
+
+
+app.set('view engine', 'ejs')
+
+
+app.use(express.urlencoded({extended :false}))
+
+
+
+
+app.get('/',(req,res)=>{
+    const articles = [{
+        title: 'Test Article',
+        createdAt: new Date(),
+        description: 'Test description',
+        posion:'testing 123',
+    }]
+    res.render('articles/index',{articles: articles})
+})
+app.use('/articles', articleRouter)
+
+app.listen(5000)
